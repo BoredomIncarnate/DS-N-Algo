544a460ea9b807751ca90f77a47cff24e2d438b5
Kyle <dev.kyoung@gmail.com>
2021-10-01 22:55:00 -0700

    added node properties to node class & cleaned up array class

--- Array.py
+++ Array.py
@@ -17,5 +17,3 @@ print(a.length)
 print(a[2])
 a[2] = 3
 print(a[2])
-print("lol")
-print("blah")
--- Node.py
+++ Node.py
@@ -1,10 +1,25 @@
 class Node():
-    def __init__():
-        self.value = None
-        self.next = None
+    def __init__(self, value=None, next=None):
+        self._value = value
+        self._next = next
 
+    def getvalue(self):
+        return self._value
 
-    def getVaule(self):
-        return self.value
+    def setvalue(self, newValue):
+        self._value = newValue
 
-    
+    def getnext(self):
+        return self._next
+
+    def setnext(self, newNext):
+        self._next = newNext
+
+    value = property(getvalue, setvalue)
+    next = property(getnext, setnext)
+
+head = Node("a")
+next = Node("b")
+head.next = next
+print(head.value)
+print(head.next.value)   
